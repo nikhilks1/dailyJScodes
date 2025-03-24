36dce96c191a424ac67edadf1b91fd3f717adc0f
nikhilks1 <nikhilks231@gmail.com>
2025-03-24 23:13:07 +0530

    day3

---
+++ positiveOrNegative.js
@@ -0,0 +1,10 @@
+function checkNumber(num){
+    if(num>=0){
+        console.log("the number is positive")
+    }
+    else{
+        console.log("the number is negative")
+    }
+}
+
+console.log(checkNumber(1));
