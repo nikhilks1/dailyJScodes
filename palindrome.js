8a848de113b8eddd9849eef2e5410111ae922940
nikhilks1 <nikhilks231@gmail.com>
2025-03-27 12:09:22 +0530

    day 6

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+function checkPalindrome(num) {
+    let rev = num.toString().split("").reverse().join("");
+    if (rev == num) {
+        console.log('This is a palindrome');
+    } else {
+        console.log('This is not a palindrome');
+    }
+    console.log(num);
+}
+
+// Example usage:
+checkPalindrome(121); // This is a palindrome
+
