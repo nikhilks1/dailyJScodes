4c7c4d06adb6a33288d68f4fccc73df5e5bcf647
nikhilks1 <nikhilks231@gmail.com>
2025-03-31 18:45:47 +0530

    just a small update

--- positiveOrNegative.js
+++ positiveOrNegative.js
@@ -1,10 +1,14 @@
 function checkNumber(num){
-    if(num>=0){
-        console.log("the number is positive")
+    if(num>0){
+        console.log("the number is Positive");
+    }
+    else if(num<0){
+        console.log("the number is Negative");
+        
     }
     else{
-        console.log("the number is negative")
+        console.log("the number is Zero");
+        
     }
-}
-
-console.log(checkNumber(1));
+ }
+ console.log(checkNumber(1)); // "The number is Positive"
