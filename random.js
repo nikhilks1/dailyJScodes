29f818afd26e3a29153ed867f9b6d442723481b9
nikhilks1 <nikhilks231@gmail.com>
2025-04-02 21:55:16 +0530

    day 12

---
+++ random.js
@@ -0,0 +1,13 @@
+let a = 200
+let b = 499
+let c = 300
+
+if(a>b&&a>c){
+    console.log("a is greater");
+}
+else if(b>a&&b>c){
+    console.log("b is greater");
+}
+else{
+    console.log("c is greater");
+}
