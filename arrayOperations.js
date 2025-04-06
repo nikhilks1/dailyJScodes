b56cc0c4e1062e67f1c5df917afc68a9ead3eeff
nikhilks1 <nikhilks231@gmail.com>
2025-04-07 01:03:59 +0530

    day 15

---
+++ arrayOperations.js
@@ -0,0 +1,11 @@
+let arr = [`a`, `b`, `c`, `d`, `e`];
+
+// accessing the elements
+console.log(arr[1]);
+
+// add an element to the end of the array
+arr.push(`f`); 
+console.log(arr); 
+
+// check if an element exists in the array
+console.log(arr.includes(`c`));
